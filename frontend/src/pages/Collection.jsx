781f17d0b8f367d698c487c37a07fecf0f7490a0
prashanna ghimire <ghimireprashanna11@gmail.com>
2026-04-09 10:53:06 +0545

    complete product page

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -5,12 +5,12 @@ import Title from "../components/Title";
 import Productitem from "../components/Productitem";
 
 const Collection = () => {
-  const { products } = useContext(ShopContext);
+  const { products, search, showSearch } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
   const [subCategory, setSubCategory] = useState([]);
-  const [sortType, setSortType] = useState('relavant')
+  const [sortType, setSortType] = useState('relevant')
 
   const toggleCategory = (e) => {
     if (category.includes(e.target.value)) {
@@ -30,6 +30,11 @@ const Collection = () => {
 
   const applyFilter = ()=>{
     let productsCopy = products.slice();
+
+    if(showSearch && search) {
+      productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
+    }
+
     if(category.length > 0){
       productsCopy = productsCopy.filter(item => category.includes(item.category))
     }
@@ -61,7 +66,7 @@ const Collection = () => {
 
   useEffect(() => {
     applyFilter()
-  },[category, subCategory]);
+  },[category, subCategory, search, showSearch]);
 
   useEffect(()=>{
     sortProduct()
