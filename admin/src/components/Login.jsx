582e21f4b0a38c593753c619d673dbca02cd6ab2
prashanna ghimire <ghimireprashanna11@gmail.com>
2026-04-26 17:20:00 +0545

    connect_backend_API_with_Admin_panel

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -1,14 +1,24 @@
+import axios from "axios";
 import React, { useState } from "react";
+import { backendUrl } from "../App";
+import { toast } from "react-toastify";
 
-const Login = () => {
+const Login = ({setToken}) => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const onSubmitHandler = async (e) =>{
         try {
             e.preventDefault();
-            console.log(email, password);
-            
+            const response = await axios.post(backendUrl + '/api/user/admin',{email,password})   
+            if(response.data.success){
+              setToken(response.data.token)
+            }else{
+              toast.error(Response.data.message)
+            }
+                    
         } catch (error) {
+            console.log(error);
+            toast.error(error.message)
             
         }
     }
