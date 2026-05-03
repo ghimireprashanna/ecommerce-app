c234a599529e342d3795fd310efc5d65881e7e31
prashanna ghimire <ghimireprashanna11@gmail.com>
2026-05-03 16:49:49 +0545

    place order feature completed

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,39 +1,67 @@
-
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js"
 
 // placing orders using Cash on delevery method
 
-const placeOrder = async(req, res)=>{
+const placeOrder = async (req, res) => {
+    try {
+        const { userId, items, amount, address } = req.body;
+        const orderData = {
+            userId, items, amount, address, paymentMethod: "COD", payment: false, date: Date.now()
+        }
+        const newOrder = new orderModel(orderData)
+        await newOrder.save()
 
+        await userModel.findByIdAndUpdate(userId, { cartData: {} })
+
+        res.json({ success: true, message: "order Placed" })
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+
+
+    }
 }
 
 
 // placing orders using Stripe method
 
-const placeOrderStripe = async(req, res)=>{
-    
+const placeOrderStripe = async (req, res) => {
+
 }
 
 
 
 // placing orders using Razorpay method
 
-const placeOrderRazorpay = async(req, res)=>{
-    
+const placeOrderRazorpay = async (req, res) => {
+
 }
 
 // All orders data for admin pannel
-const allOrders = async(req, res)=>{
-    
+const allOrders = async (req, res) => {
+
 }
 
 // user orders data for frontend
-const userOrders = async(req, res)=>{
-    
+const userOrders = async (req, res) => {
+    try {
+        const { userId } = req.body
+
+        const orders = await orderModel.find({ userId })
+        res.json({ success: true, orders })
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+
+
+    }
 }
 
 // update order status
-const updateStatus = async(req, res)=>{
-    
+const updateStatus = async (req, res) => {
+
 }
 
-export { placeOrder,placeOrderStripe, placeOrderRazorpay,allOrders,userOrders,updateStatus }
+export { placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus }
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { placeOrder, placeOrderRazorpay,placeOrderStripe,allOrders, userOrders, updateStatus } from '../controllers/orderController'
+import { placeOrder, placeOrderRazorpay,placeOrderStripe,allOrders, userOrders, updateStatus } from '../controllers/orderController.js'
 import adminAuth from '../middleware/adminAuth.js'
 import authUser from '../middleware/auth.js'
 
